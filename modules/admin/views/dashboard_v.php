9757a6bd6f4d29d0b695c2fcf504473899e2910c
ReinzKrieger <62374195+ReinzKrieger@users.noreply.github.com>
2020-03-28 20:50:37 +0700

    Add files via upload

--- modules/admin/views/dashboard_v.php
+++ modules/admin/views/dashboard_v.php
@@ -17,19 +17,7 @@
           <div class="content-wrapper">
               <!-- Content Header (Page header) -->
               <div class="content-header">
-                  <div class="container-fluid">
-                      <div class="row mb-2">
-                          <div class="col-sm-6">
-                              <h1 class="m-0 text-dark">Dashboard v3</h1>
-                          </div><!-- /.col -->
-                          <div class="col-sm-6">
-                              <ol class="breadcrumb float-sm-right">
-                                  <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                  <li class="breadcrumb-item active">Dashboard v3</li>
-                              </ol>
-                          </div><!-- /.col -->
-                      </div><!-- /.row -->
-                  </div><!-- /.container-fluid -->
+                  <?php $this->load->view("admin/_part/breadcrumb.php") ?>
               </div>
               <!-- /.content-header -->
 
@@ -293,20 +281,8 @@
           <!-- /.control-sidebar -->
 
           <!-- Main Footer -->
-          <footer class="main-footer">
-              <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
-              All rights reserved.
-              <div class="float-right d-none d-sm-inline-block">
-                  <b>Version</b> 3.0.2
-              </div>
-          </footer>
+          <?php $this->load->view("admin/_part/footer.php") ?>
       </div>
-      <!-- ./wrapper -->
-      <!-- OPTIONAL SCRIPTS -->
-      <script src="plugins/chart.js/Chart.min.js"></script>
-      <script src="dist/js/demo.js"></script>
-      <script src="dist/js/pages/dashboard3.js"></script>
-
       <!-- jQuery -->
       <script src="<?php echo base_url('../assets/plugins/jquery/jquery.min.js') ?>"></script>
       <!-- Bootstrap 4 -->
